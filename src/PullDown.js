38cf40f3e7175ad1ed357e5c758a92a02e557fdf
Eiji Yamamoto <yamamoto-e@swlab.cs.okayama-u.ac.jp>
2018-07-02 17:53:35 +0900

    Modify PullDown class by state to dynamically change

--- src/PullDown.js
+++ src/PullDown.js
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const PullDown = (props) => {
-	const items = props.data.map(v => {
-		return <option value={v}>{v}</option>;
-	});
+class PullDown extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			value: this.props.data[0]
+		};
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
 
-	return (
-			<div>
-			<select>{items}</select>
+	handleChange(event){
+		this.setState({
+			value: event.target.value
+		})
+	}
+
+	handleSubmit(event) {
+		alert('Slected column is: ' + this.state.value);
+		event.preventDefault();
+	}
+
+	render() {
+		const items = this.props.data.map(v => {
+			return <option value={v} key={v}>{v}</option>;
+		});
+
+		return (
+				<form onSubmit={this.handleSubmit}>
+				<label>
+				Pick any column:
+				<select value={this.state.value} onChange={this.handleChange}>
+				{items}
+			</select>
+				</label>
+				<input type="submit" value="Submit" />
+				<div>
+				{this.state.value}
 			</div>
-	);
+				</form>
+		);
+	}
 }
-
 export default PullDown;
