805ea9698084d991e0d72155843bbbed89fc3d83
Eiji Yamamoto <yamamoto-e@swlab.cs.okayama-u.ac.jp>
2018-07-02 23:57:29 +0900

    Add methods method in FilterCollection class

--- src/FilterCollection.js
+++ src/FilterCollection.js
@@ -14,7 +14,7 @@ export default class FilterCollection{
 			 "condition":"match:summary:.",
 			 "modifier":"replace:location:"}
 		];
-		this.collection = collection
+		this.collection = collection;
 	}
 
 	values(key){
@@ -23,7 +23,19 @@ export default class FilterCollection{
 			values.push(hash[key]);
 		});
 		return values;
+	}
 
-
+	methods(){
+		var methods = [];
+		this.collection.map(hash => {
+			var condition_method = hash["condition"].split(":")[0];
+			var modifier_method = hash["modifier"].split(":")[0];
+			methods.push(condition_method);
+			methods.push(modifier_method);
+		});
+		methods = methods.filter(function(x,i,self){
+			return self.indexOf(x) === i;
+		});
+		return methods;
 	}
 }
