adcc1e24eeb2723a63a1ee26e3b7252346309730
Eiji Yamamoto <yamamoto-e@swlab.cs.okayama-u.ac.jp>
2018-07-02 21:27:48 +0900

    Modify PageOfCorrespondenceTable class to Container component
    PresentationalなcomponentであるPullDownとSubmitButtonを利用してページを描画するように，Container componentを意識して編集した．
    これにより，対応表のページに必要な要素は作成できたと考えられる．
    なお，動的にページの描画を変化させるコードは，{/* ... */}のコメントを利用して記述している．
    今後は，フィルタやアクションの編集画面に取り掛かる．
    このために，各画面のようのクラスと，各collectionで，要素をparseするメソッドを作る必要がある．

--- src/PageOfCorrespondenceTable.js
+++ src/PageOfCorrespondenceTable.js
@@ -2,21 +2,60 @@ import React, { Component } from 'react';
 import ActionCollection from './ActionCollection';
 import FilterCollection from './FilterCollection';
 import PullDown from './PullDown';
+import SubmitButton from './SubmitButton';
 
 class PageOfCorrespondenceTable extends Component{
-	render(){
+	constructor() {
+		super();
 		var fc = new FilterCollection();
-		var f_names = fc.values("name");
 		var ac = new ActionCollection();
+		var f_names = fc.values("name");
 		var a_names = ac.values("name");
+		this.state = {
+			f_names: f_names,
+			a_names: a_names,
+			f_name: f_names[0],
+			a_name: a_names[0]
+		};
+		this.handleFilterChange = this.handleFilterChange.bind(this);
+		this.handleActionChange = this.handleActionChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
 
+	handleFilterChange(event){
+		this.setState({
+			f_name: event.target.value
+		});
+	}
+
+	handleActionChange(event){
+		this.setState({
+			a_name: event.target.value
+		});
+	}
+
+	handleSubmit(event) {
+        alert('CorrespondenceTable: \nfilter: ' + this.state.f_name + '\naction: ' + this.state.a_name);
+        event.preventDefault();
+    }
+
+	render(){
 		return(
-				<div>
-				Filters
-				<PullDown data = {f_names}/>
-				Actions
-				<PullDown data = {a_names}/>
-				</div>
+			<div>
+			  Filters
+			  <br/>
+			  <PullDown data = {this.state.f_names} handleChange = {this.handleFilterChange}/>
+			  <p/>
+			  Actions
+			  <br/>
+			  <PullDown data = {this.state.a_names} handleChange = {this.handleActionChange}/>
+			  <br/>
+			  <SubmitButton handleSubmit = {this.handleSubmit}/>
+			  {/*
+				  <br/>
+					  CorrespondenceTable is {this.state.f_name}, {this.state.a_name}
+				  */}
+			</div>
 		);
 	}
 }
