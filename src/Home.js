a02d664c1d4066809fca48e716b2b555b4514166
Eiji Yamamoto <yamamoto-e@swlab.cs.okayama-u.ac.jp>
2018-07-04 17:06:55 +0900

    Add Home of Presentational component

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const Home = (props) => {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to goohub-GUI</h1>
+        </header>
+		</div>
+    );
+}
+
+export default Home;
