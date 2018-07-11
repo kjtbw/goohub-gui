299f3444602c31e76c9e57d7571e89170de529c4
Eiji Yamamoto <yamamoto-e@swlab.cs.okayama-u.ac.jp>
2018-07-11 12:30:50 +0900

    Add opponent column in action
    今後の処置を考え，利用しそうなメソッドをコメントで追加した．
    実際追加するかは未確定である．

--- src/ActionCollection.js
+++ src/ActionCollection.js
@@ -3,22 +3,26 @@ export default class ActionCollection{
 		const collection =
 			  [{"id":"1",
 				"name":"stdout",
+                "opponent":"stdout",
 				"converter":"convert_sentence",
 				"informant":"inform_stdout"},
 			   {"id":"2",
 				"name":"slack",
+                "opponent":"slack",
 				"converter":"convert_sentence",
 				"informant":"inform_slack"},
 			   {"id":"3",
 				"name":"calendar",
+                "opponent":"calendar",
 				"converter":"convert_google_event",
 				"informant":"inform_calendar"},
 			   {"id":"4",
 				"name":"mail",
+                "opponent":"mail",
 				"converter":"convert_sentence",
 				"informant":"inform_mail"}
 			  ];
-		this.collection = collection
+		this.collection = collection;
 	}
 
 	values(key){
@@ -28,5 +32,29 @@ export default class ActionCollection{
 		});
 		return values;
 	}
+
+    // add(hash){
+    //     this.collection.push(hash);
+    // }
+
+    // get_converter(name){
+    //     var converter;
+    //     this.collection.map(hash =>{
+    //         if(hash["name"] == name){
+    //             converter = hash["converter"];
+    //         }
+    //     });
+    //     return converter;
+    // }
+
+    // get_informant(name){
+    //     var informant;
+    //     this.collection.map(hash =>{
+    //         if(hash["name"] == name){
+    //             informant = hash["informant"];
+    //         }
+    //     });
+    //     return informant;
+    // }
 
 }
