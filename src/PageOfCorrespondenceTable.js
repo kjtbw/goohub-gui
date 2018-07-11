07f5b7c57bf41b2b86ec644316bb21c8cc22c6b1
Eiji Yamamoto <yamamoto-e@swlab.cs.okayama-u.ac.jp>
2018-07-11 20:59:25 +0900

    Modify PageOfCorrespondenceTable to display CorrespondenceTable name dynamically
    本コミットをもって，フィルタ，アクション，対応表で，とりあえず名前を表示することはできた
    + 今後の課題
    + jsonからの読み出し
    + jsonへの書き出し(今のnamesへのpush相当)
    + 各フィルタなどの詳細の表示(Tableを作るクラスをつくって，そこに表示させるのがよさそう)

--- src/PageOfCorrespondenceTable.js
+++ src/PageOfCorrespondenceTable.js
@@ -3,6 +3,8 @@ import ActionCollection from './ActionCollection';
 import FilterCollection from './FilterCollection';
 import PullDown from './PullDown';
 import SubmitButton from './SubmitButton';
+import List from './List';
+import TextBox from './TextBox';
 
 class PageOfCorrespondenceTable extends Component{
 	constructor() {
@@ -12,6 +14,9 @@ class PageOfCorrespondenceTable extends Component{
 		var f_names = fc.values("name");
 		var a_names = ac.values("name");
 		this.state = {
+            name: "",
+            calendar_id: "",
+            ct_names: [],
 			f_names: f_names,
 			a_names: a_names,
 			f_name: f_names[0],
@@ -19,6 +24,8 @@ class PageOfCorrespondenceTable extends Component{
 		};
 		this.handleFilterChange = this.handleFilterChange.bind(this);
 		this.handleActionChange = this.handleActionChange.bind(this);
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handleCIDChange = this.handleCIDChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
 	}
 
@@ -34,14 +41,39 @@ class PageOfCorrespondenceTable extends Component{
 		});
 	}
 
+	handleNameChange(event){
+		this.setState({
+			name: event.target.value
+		});
+	}
+
+	handleCIDChange(event){
+		this.setState({
+			calendar_id: event.target.value
+		});
+	}
+
 	handleSubmit(event) {
-        alert('CorrespondenceTable: \nfilter: ' + this.state.f_name + '\naction: ' + this.state.a_name);
+        alert('CorrespondenceTable: \nname: ' + this.state.name +
+              '\ncalendar id: ' + this.state.calendar_id +
+              '\nfilter: ' + this.state.f_name +
+              '\naction: ' + this.state.a_name);
+        this.state.ct_names.push(this.state.name);
+        this.setState(this.state);
         event.preventDefault();
     }
 
 	render(){
 		return(
 			<div>
+              Name
+              <TextBox handleTextChange = {this.handleNameChange}/>
+              <p/>
+
+              Calendar ID
+              <TextBox handleTextChange = {this.handleCIDChange}/>
+              <p/>
+
 			  Filters
 			  <br/>
 			  <PullDown data = {this.state.f_names} handleChange = {this.handleFilterChange}/>
@@ -51,10 +83,10 @@ class PageOfCorrespondenceTable extends Component{
 			  <PullDown data = {this.state.a_names} handleChange = {this.handleActionChange}/>
 			  <br/>
 			  <SubmitButton handleSubmit = {this.handleSubmit}/>
-			  {/*
-				  <br/>
-					  CorrespondenceTable is {this.state.f_name}, {this.state.a_name}
-				  */}
+              <p/>
+              CorrespondenceTables
+              <br/>
+              <List data = {this.state.ct_names}/>
 			</div>
 		);
 	}
