db0896738e48f849e10687c7b8c7504b22a93915
Eiji Yamamoto <yamamoto-e@swlab.cs.okayama-u.ac.jp>
2018-07-25 14:53:05 +0900

    Adjust PageOfCalendar layout

--- src/PageOfCalendar.js
+++ src/PageOfCalendar.js
@@ -4,8 +4,9 @@ import moment from 'moment';
 import events from './events';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import {Grid, Row, Col} from 'react-bootstrap';
 
-moment.locale('en-GB');
+moment.locale('ja-JP');
 BigCalendar.momentLocalizer(moment);
 
 const allViews = Object.keys(BigCalendar.Views).map(k => BigCalendar.Views[k]);
@@ -13,8 +14,24 @@ const allViews = Object.keys(BigCalendar.Views).map(k => BigCalendar.Views[k]);
 class PageOfCalendar extends Component{
     render(){
 		return(
-            <div style={{ height: 700 }}>
-              <Link to="/filter">フィルタを作る</Link>&nbsp;
+            <div style={{height: 625}}>
+              <Grid>
+                <Row>
+                  <Col xs={12} md={8}>
+                    <h1>Calendar</h1>
+                  </Col>
+                  <br/>
+                  <Col xs={6} md={4}>
+                    <div align="right">
+                      <h4>
+                        <Link to="/filter">新しいフィルタを作成</Link>&nbsp;
+                      </h4>
+                    </div>
+                  </Col>
+                </Row>
+              </Grid>
+              <p/>
+
               <BigCalendar
                 events={events}
                 step={60}
