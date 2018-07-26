06da15ffbbf0e600de141ace1e6f7b6aa4befd49
Eiji Yamamoto <yamamoto-e@swlab.cs.okayama-u.ac.jp>
2018-07-26 10:30:35 +0900

    Add event tag

--- src/events.js
+++ src/events.js
@@ -2,61 +2,72 @@ export default [
     {
         'title': '打合せ',
         'start': new Date(2018, 7, 31, 10, 0, 0),
-        'end': new Date(2018, 7, 31, 12, 0, 0)
+        'end': new Date(2018, 7, 31, 12, 0, 0),
+        'tag': 'office'
     },
     {
         'title': '同窓会',
         'start': new Date(2018, 7, 18, 19, 0, 0),
-        'end': new Date(2018, 7, 18, 22, 0, 0)
+        'end': new Date(2018, 7, 18, 22, 0, 0),
+        'tag': 'personal'
     },
 
     {
         'title': '出張',
         'start': new Date(2018, 7, 20, 0, 0, 0),
-        'end': new Date(2018, 7, 22, 0, 0, 0)
+        'end': new Date(2018, 7, 22, 0, 0, 0),
+        'tag': 'office'
     },
 
     {
         'title': '通院',
         'start': new Date(2018, 7, 9, 14, 0, 0),
-        'end': new Date(2018, 7, 9, 16, 0, 0)
+        'end': new Date(2018, 7, 9, 16, 0, 0),
+        'tag': 'personal'
     },
     {
         'title': '講義A',
         'start': new Date(2018, 7, 2, 9, 0, 0),
         'end': new Date(2018, 7, 2, 11, 0, 0),
+        'tag': 'lecture'
     },
     {
         'title': '講義A',
         'start': new Date(2018, 7, 9, 9, 0, 0),
         'end': new Date(2018, 7, 9, 11, 0, 0),
+        'tag': 'lecture'
     },
     {
         'title': '定例会議',
         'start': new Date(2018, 7, 6, 10, 30, 0, 0),
         'end': new Date(2018, 7, 6, 12, 30, 0, 0),
-        desc: 'Pre-meeting meeting, to prepare for the meeting'
+        desc: 'Pre-meeting meeting, to prepare for the meeting',
+        'tag': 'office'
     },
     {
         'title': '昼食会',
         'start': new Date(2018, 7, 12, 12, 0, 0, 0),
         'end': new Date(2018, 7, 12, 13, 0, 0, 0),
-        desc: 'Power lunch'
+        desc: 'Power lunch',
+        'tag': 'personal'
     },
     {
         'title': '定例会議',
         'start': new Date(2018, 7, 23, 14, 0, 0, 0),
-        'end': new Date(2018, 7, 23, 15, 0, 0, 0)
+        'end': new Date(2018, 7, 23, 15, 0, 0, 0),
+        'tag': 'office'
     },
     {
         'title': '夕食会',
         'start': new Date(2018, 7, 4, 20, 0, 0, 0),
-        'end': new Date(2018, 7, 4, 21, 0, 0, 0)
+        'end': new Date(2018, 7, 4, 21, 0, 0, 0),
+        'tag': 'personal'
     },
     {
         'title': '盆休み',
         'allDay': true,
         'start': new Date(2018, 7, 13),
-        'end': new Date(2018, 7, 15)
+        'end': new Date(2018, 7, 15),
+        'tag': 'personal'
     }
 ]
