7bee0f4cf331581c1fca67b0c2b49b2b76b27de4
Eiji Yamamoto <yamamoto-e@swlab.cs.okayama-u.ac.jp>
2019-06-28 16:49:28 +0900

    Add draft function to read JSON file and to put event
    JSONファイルからデータを読んで，それをeventに変換して，BigCalendarに追加する機能を作った．
    TODO: goohubからgoohub-GUIへのファイルの移動
    goohub/dbにあるファイルを読もうとしたら，srcの外!って怒られた
    goohubにUIのためのinitコマンドを作るか，UIがgoohub/dbを読めるようにするか
    この辺をやる必要あり
    TODO: 複数ファイルのread
    TODO: ファイルの拡張子
    上記の設定するとき，ファイルに.jsonって拡張子つけたい

--- src/PageOfCalendar.js
+++ src/PageOfCalendar.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import BigCalendar from 'react-big-calendar';
 import moment from 'moment';
-import events from './events';
+//import events from './events';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import {Grid, Row, Col, Button} from 'react-bootstrap';
@@ -14,8 +14,26 @@ const allViews = Object.keys(BigCalendar.Views).map(k => BigCalendar.Views[k]);
 class PageOfCalendar extends Component{
     constructor(){
         super();
+        //        var raw_data = JSON.parse(JSON.stringify(data));
+        //      console.log(raw_data)
+
+        var data = require('your_json_data_path')
+        var events = [];
+        for(var i = 0; i < data.items.length; i++) {
+            var e = data.items[i];
+            console.log(e);
+            e = {
+                'title': e.summary,
+                'start': new Date(e.start.date_time),
+                'end': new Date(e.end.date_time),
+                'tag': 'office'
+            };
+            events.push(e);
+        }
+
         this.state = {
-            filter:""
+            filter:"",
+            events: events
         };
     }
     render(){
@@ -26,14 +44,15 @@ class PageOfCalendar extends Component{
         if (this.props.match.params.id.includes("undefined")){
             this.props.match.params.id = "なし";
         }
-        // filter1なら，会議を仕事で置換
-        if (this.props.match.params.id.includes("会議を仕事で置換")){
-            events.map(e =>{
-                if(e["title"].includes("会議"))
-                    e["title"] = "仕事";
-            });
-            this.state.filter = "(フィルタ: 会議を仕事で置換)";
-        }
+        //filter1なら，会議を仕事で置換
+        // if (this.props.match.params.id.includes("会議を仕事で置換")){
+        //     events.map(e =>{
+        //         if(e["title"].includes("会議"))
+        //             e["title"] = "仕事";
+        //         //                e["desc"] = "";
+        //     });
+        //     this.state.filter = "(フィルタ: 会議を仕事で置換)";
+        // }
         // filter3なら，出張を仕事で置換
         // if (this.props.match.params.id.includes("filter3")){
         //     events.map(e =>{
@@ -52,94 +71,110 @@ class PageOfCalendar extends Component{
         //     });
         //}
 
-    return(
-        <div style={{height: 625}}>
-          <Grid>
-            <Row>
-              <Col xs={16} md={12}>
-                <h1>山本先生のカレンダ{this.state.filter}</h1>
-              </Col>
-              <br/>
-              <Col xs={6} md={4}>
-                <div align="left">
-                  <h4>
-                    <Link to="/filter">
-                      <Button>
-                        新しいフィルタを作成
-                      </Button>
-                    </Link>&nbsp;
-                  </h4>
-                </div>
-              </Col>
-            </Row>
-          </Grid>
-          <p/>
+        return(
+            <div style={{height: 625}}>
+              <Grid>
+                <Row>
+                  <Col xs={16} md={12}>
+                    <h1>山本先生のカレンダ{this.state.filter}</h1>
+                  </Col>
+                </Row>
 
-          <BigCalendar
-            // if you want to set onclick event, use this code
-            // selectable
-            // onSelectEvent={event => alert(event.title)}
-            events={events}
-            step={60}
-            views={allViews}
-            defaultDate={new Date(2018, 7, 24)}
-            eventPropGetter={
-                (event, start, end, isSelected) => {
-                    if (event["tag"] == "lecture"){
-                        let lecture = {
-                            backgroundColor: "lightblue",
-                            color: 'black',
-                            borderRadius: "0px",
-                            border: "none"
-                        };
-                        return {
-                            className: "",
-                            style: lecture
-                        };
-                    }else{
-                        if (event["tag"] == "office"){
-                            let office = {
-                                backgroundColor: "lightgreen",
+                <Row>
+                  <Col xs={6} md={4}>
+                    <div align="left">
+                      <h4>
+                        <Link to="/filter">
+                          <Button>
+                            新しいフィルタを作成
+                          </Button>
+                        </Link>&nbsp;
+                      </h4>
+                    </div>
+                  </Col>
+
+
+                  <Col xs={6} md={4}>
+                    <div align="left">
+                      <h4>
+                        <Link to={"/action/" + this.props.match.params.id}>
+                          <Button>
+                            新しいアクションを作成
+                          </Button>
+                        </Link>&nbsp;
+                      </h4>
+                    </div>
+                  </Col>
+                </Row>
+              </Grid>
+              <p/>
+              <BigCalendar
+                // if you want to set onclick event, use this code
+                // selectable
+                // onSelectEvent={event => alert(event.title)}
+                events={this.state.events}
+                step={60}
+                views={allViews}
+                defaultDate={new Date()}
+                //            onSelectEvent={event =>alert("予定名: " + event.title + "\n" +
+                //                                       "説明: " + event.desc)}
+                eventPropGetter={
+                    (event, start, end, isSelected) => {
+                        if (event["tag"] == "lecture"){
+                            let lecture = {
+                                backgroundColor: "lightblue",
                                 color: 'black',
                                 borderRadius: "0px",
                                 border: "none"
                             };
                             return {
                                 className: "",
-                                style: office
+                                style: lecture
                             };
                         }else{
-                            if (event["tag"] == "personal"){
-                                let personal = {
-                                    backgroundColor: "lightpink",
+                            if (event["tag"] == "office"){
+                                let office = {
+                                    backgroundColor: "lightgreen",
                                     color: 'black',
                                     borderRadius: "0px",
                                     border: "none"
                                 };
                                 return {
                                     className: "",
-                                    style: personal
+                                    style: office
                                 };
-                            }else {
-                                let others = {
-                                    backgroundColor: "lightgrey",
-                                    color: 'black',
-                                    borderRadius: "0px",
-                                    border: "none"
-                                };
-                                return {
-                                    className: "",
-                                    style: others
-                                };
+                            }else{
+                                if (event["tag"] == "personal"){
+                                    let personal = {
+                                        backgroundColor: "lightpink",
+                                        color: 'black',
+                                        borderRadius: "0px",
+                                        border: "none"
+                                    };
+                                    return {
+                                        className: "",
+                                        style: personal
+                                    };
+                                }else {
+                                    let others = {
+                                        backgroundColor: "lightgrey",
+                                        color: 'black',
+                                        borderRadius: "0px",
+                                        border: "none"
+                                    };
+                                    return {
+                                        className: "",
+                                        style: others
+                                    };
+                                }
                             }
                         }
                     }
                 }
-            }
-            />
-        </div>
-    );
-}
+                />
+            </div>
+        );
+    }
 }
 
 export default PageOfCalendar;
