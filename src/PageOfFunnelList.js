869a56f02f8d1ac78eb9380ce336734abe646f3d
Eiji Yamamoto <yamamoto-e@swlab.cs.okayama-u.ac.jp>
2019-07-29 14:45:50 +0900

    Use user made funnel info in FunnelList page

--- src/PageOfFunnelList.js
+++ src/PageOfFunnelList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import ObjectList from './ObjectList';
+import { BrowserRouter as  Link } from 'react-router-dom';
+import {Button, Grid} from 'react-bootstrap';
 
 class PageOfFunnelList extends Component{
 	constructor() {
@@ -41,33 +43,47 @@ class PageOfFunnelList extends Component{
 	render(){
 		return(
 			<div>
-              {/* 案1の構成だと，既存のフィルタを管理する必要がないので，この辺の情報はいらない
-              FunnelList
-              <br/>
-              <ArrayObjectList data = {this.state.funnels}/>
-              FilterList
-              <br/>
-              <ArrayObjectList data = {this.state.filters}/>
-              ActionList
-              <br/>
-              <ArrayObjectList data = {this.state.actions}/>
-              OutletList
-              <br/>
-              <ArrayObjectList data = {this.state.outlets}/>
-              */}
-              RuleList
-              <br/>
-              <ol>
-                {this.state.rule_list.map((rule) => (
-                    <li>
-                      {rule.name}:<br/>
-                      filter:<ObjectList data = {rule.filter}/>
-                      action:<ObjectList data = {rule.action}/>
-                      outlet:<ObjectList data = {rule.outlet}/>
-                    </li>
-                ))}
+              <Grid>
+                {/* 案1の構成だと，既存のフィルタを管理する必要がないので，この辺の情報はいらない
+                    FunnelList
+                    <br/>
+                        <ArrayObjectList data = {this.state.funnels}/>
+                            FilterList
+                            <br/>
+                                <ArrayObjectList data = {this.state.filters}/>
+                                    ActionList
+                                    <br/>
+                                        <ArrayObjectList data = {this.state.actions}/>
+                                            OutletList
+                                            <br/>
+                                                <ArrayObjectList data = {this.state.outlets}/>
+                                                */}
+                                                Your Rule
+                                                <p/>
+                                                filter: {this.props.location.state.filter_dsl}<br/>
+                                                action: {this.props.location.state.action_dsl}<br/>
+                                                outlet: {this.props.location.state.outlet_dsl}
+                                                <p/>
+
+                                                RuleList
+                                                <br/>
+                                                <ol>
+                                                  {this.state.rule_list.map((rule) => (
+                                                      <li>
+                                                        {rule.name}:<br/>
+                                                        filter:<ObjectList data = {rule.filter}/>
+                                                        action:<ObjectList data = {rule.action}/>
+                                                        outlet:<ObjectList data = {rule.outlet}/>
+                                                      </li>
+                                                  ))}
             </ol>
-			</div>
+                <Link to="/calendar/">
+                <Button>
+                カレンダへ戻る
+            </Button>
+                </Link>&nbsp;
+            </Grid>
+			    </div>
 		);
 	}
 }
