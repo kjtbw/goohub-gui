73ba8b5daceb857443b6451ac16bf9417d344c67
Eiji Yamamoto <yamamoto-e@swlab.cs.okayama-u.ac.jp>
2019-08-09 13:39:27 +0900

    Add PaegOfMakeRuleName

---
+++ src/PageOfMakeRuleName.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import TextBox from './TextBox';
+import List from './List';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Container  from 'react-bootstrap/Container';
+
+class PageOfMakeRuleName extends Component{
+	constructor() {
+		super();
+		this.state = {
+            name: ""
+		};
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handleNext = this.handleNext.bind(this);
+	}
+
+    handleNameChange(event){this.setState({name: event.target.value});}
+    handleNext(){
+        this.props.history.push({
+            pathname: "/make/filter",
+            state: { name: this.state.name,
+                   }, });}
+
+	render(){
+		return(
+			<div>
+              <Container>
+                <h1>Rule Name</h1>
+                作成するルール名: <TextBox handleTextChange = {this.handleNameChange}/>
+                <p/>
+
+                <h4>
+                  <Link to="/calendar/">
+                    <Button>
+                      カレンダへ
+                    </Button>
+                  </Link>&nbsp;
+
+                  <Button onClick={this.handleNext}>
+                    フィルタの設定へ
+                  </Button>
+                </h4>
+              </Container>
+			</div>
+		);
+	}
+}
+
+export default PageOfMakeRuleName;
